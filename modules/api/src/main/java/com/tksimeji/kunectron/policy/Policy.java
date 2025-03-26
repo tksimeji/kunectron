7bebabe9089fc7fb1e33a9300a4294cc966ebde4
tksimeji <tksimeji@outlook.com>
2025-03-26 09:49:00 +0900

    fix: fixed argument name for item slot policy creation method

--- modules/api/src/main/java/com/tksimeji/kunectron/policy/Policy.java
+++ modules/api/src/main/java/com/tksimeji/kunectron/policy/Policy.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.NotImplementedException;
 import org.jetbrains.annotations.NotNull;
 
 public interface Policy {
-    static @NotNull ItemSlotPolicy itemSlot(final boolean fixation) {
+    static @NotNull ItemSlotPolicy itemSlot(final boolean variation) {
         throw new NotImplementedException("The API module cannot be called at runtime.");
     }
 }
