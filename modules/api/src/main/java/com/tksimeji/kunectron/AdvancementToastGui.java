4b6eb344e8fbb31c26248b645c93cf8431143283
tksimeji <tksimeji@outlook.com>
2025-04-08 17:18:59 +0900

    chore: added experimental annotations to the advancement toast GUI

--- modules/api/src/main/java/com/tksimeji/kunectron/AdvancementToastGui.java
+++ modules/api/src/main/java/com/tksimeji/kunectron/AdvancementToastGui.java
@@ -1,10 +1,13 @@
 package com.tksimeji.kunectron;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@ApiStatus.Experimental
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface AdvancementToastGui {
