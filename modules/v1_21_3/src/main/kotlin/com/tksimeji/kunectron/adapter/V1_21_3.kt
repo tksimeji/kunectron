367a90df9f918c01b0832f3ee8ba6a22f260c336
tksimeji <tksimeji@outlook.com>
2025-04-13 16:17:40 +0900

    fix: fixed problem with 1.21.3 adapter not enabling or disabling additional tooltips

--- modules/v1_21_3/src/main/kotlin/com/tksimeji/kunectron/adapter/V1_21_3.kt
+++ modules/v1_21_3/src/main/kotlin/com/tksimeji/kunectron/adapter/V1_21_3.kt
@@ -1,6 +1,9 @@
 package com.tksimeji.kunectron.adapter
 
 import io.papermc.paper.datacomponent.DataComponentTypes
+import org.bukkit.NamespacedKey
+import org.bukkit.Registry
+import org.bukkit.attribute.AttributeModifier
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
@@ -14,12 +17,21 @@ object V1_21_3: V1_21_x() {
     }
 
     override fun hideAdditionalTooltip(itemStack: ItemStack, plugin: JavaPlugin) {
-        itemStack.addItemFlags(*ItemFlag.entries.toTypedArray())
-        itemStack.setData(DataComponentTypes.HIDE_ADDITIONAL_TOOLTIP)
+        val itemMeta = itemStack.itemMeta
+        itemMeta.addItemFlags(*ItemFlag.entries.toTypedArray())
+        for (attribute in Registry.ATTRIBUTE) {
+            itemMeta.removeAttributeModifier(attribute)
+            itemMeta.addAttributeModifier(attribute, AttributeModifier(NamespacedKey(plugin, attribute.key.key), 0.0, AttributeModifier.Operation.ADD_NUMBER))
+        }
+        itemStack.itemMeta = itemMeta
     }
 
     override fun showAdditionalTooltip(itemStack: ItemStack, plugin: JavaPlugin) {
         itemStack.removeItemFlags(*ItemFlag.entries.toTypedArray())
-        itemStack.unsetData(DataComponentTypes.HIDE_ADDITIONAL_TOOLTIP)
+        val itemMeta = itemStack.itemMeta
+        for (attribute in Registry.ATTRIBUTE) {
+            itemMeta.removeAttributeModifier(attribute)
+        }
+        itemStack.itemMeta = itemMeta;
     }
 }
